3064e8b47368ee5ed435469e7dde6027e04576b3
PP <www.pzana@hotmail.co.th>
2023-02-10 14:27:02 +0700

    v1 test upload

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { StripeApp } from './src/StripeApp';
+import { StripeProvider } from '@stripe/stripe-react-native';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider
+      publishableKey='pk_test_51MZcCtCZp5yHCVLLauwAwoC54CoJZS0j7xt974JeCBl85RfpR4NPF1JEmBFF7AxkXjrFj6Lp7luD47K4Jo5nuITW00ca68xEpx'
+    >
+      <StripeApp/>
+    </StripeProvider>
+    
   );
 }
 
